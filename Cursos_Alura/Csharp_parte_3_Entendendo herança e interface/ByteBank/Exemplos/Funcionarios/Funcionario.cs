d52ac9cc293e7bc04a046c979cc13f9784a6f4f8
Marcelo <marcelosj7159@gmail.com>
2022-03-05 14:17:26 -0300

    Concluido curso sobre heranças

---
+++ Cursos_Alura/Csharp_parte_3_Entendendo herança e interface/ByteBank/Exemplos/Funcionarios/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+        public static int TotalDeFuncionarios { get; private set; }
+        
+        public Funcionario(double salario, string cpf)
+        {
+            Salario = salario;
+            CPF = cpf;
+            Console.WriteLine("Criando funcionario");
+            TotalDeFuncionarios++;
+        }
+
+        public abstract double GetBonificacao();
+
+        public abstract void AumentarSalario();
+        
+
+    }
+}
---
+++ Cursos_Alura/Csharp_parte_3_Entendendo herança e interface/ByteBank/IgorExemploAtividade/Classes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgorExemploAtividade
+{
+    public class Classes
+    {
+        public interface IAutenticavel
+        {
+            bool Autenticar(string senha);
+        }
+
+        public interface IBonificavel
+        {
+            double GetBonificacao();
+        }
+
+        public abstract class PrestadorDeServicos : IBonificavel
+        {
+            public string CNPJ { get; set; }
+
+            public double GetBonificacao()
+            {
+                return 2000.0;
+            }
+        }
+
+        public class Arquiteto : PrestadorDeServicos, IAutenticavel
+        {
+            public string Senha { get; set; }
+
+            public bool Autenticar(string senha)
+            {
+                return Senha == senha;
+            }
+        }
+    }
+}
