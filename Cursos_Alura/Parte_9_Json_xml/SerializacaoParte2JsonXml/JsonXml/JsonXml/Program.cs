89d19b9ed02a1aa015d36f415a3a03deb1292359
Marcelo <marcelosj7159@gmail.com>
2022-03-20 07:45:54 -0300

    Adicionando projeto 8 e parte do 9

---
+++ Cursos_Alura/Parte_9_Json_xml/SerializacaoParte2JsonXml/JsonXml/JsonXml/Program.cs
@@ -0,0 +1,155 @@
+﻿
+using Nancy.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JsonXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loja = ObterDados();
+            var javascriptSerializer = new JavaScriptSerializer();
+            var json = javascriptSerializer.Serialize(loja);
+
+            Console.WriteLine(json);
+
+            using (var streamWriter = new StreamWriter("Loja.json"))
+            {
+                streamWriter.Write(json);
+            }
+
+            var deserializeJson = (LojaDeFilmes)javascriptSerializer.Deserialize<LojaDeFilmes>(json);
+
+            foreach (var filme in deserializeJson.Filmes)
+            {
+                Console.WriteLine(filme.Titulo);
+            }
+
+            /// SEgunda parte
+            /// 
+            var jsonConvertido = JsonConvert.SerializeObject(loja);
+            Console.WriteLine(jsonConvertido);
+
+            var objetoDeserializado = JsonConvert.DeserializeObject<LojaDeFilmes>(jsonConvertido);
+
+            foreach (var filme in objetoDeserializado.Filmes)
+            {
+                Console.WriteLine(filme.Titulo);
+            }
+
+            // Movie Store
+            var movieStore = JsonConvert.DeserializeObject<MovieStore>(jsonConvertido);
+            
+            foreach (var filme in movieStore.Movies)
+            {
+                Console.WriteLine(filme.Title);
+            }
+
+        }
+
+        private static LojaDeFilmes ObterDados()
+        {
+            return new LojaDeFilmes
+            {
+                Diretores = new List<Diretor>
+                {
+                    new Diretor { Nome = "James Cameron", NumeroFilmes = 5 },
+                    new Diretor { Nome = "Francis Lawrence", NumeroFilmes = 3 },
+                    new Diretor { Nome = "Zack Snyder", NumeroFilmes = 6 },
+                    new Diretor { Nome = "Joss Whedon", NumeroFilmes = 7 },
+                    new Diretor { Nome = "Martin Scorsese", NumeroFilmes = 4 },
+                    new Diretor { Nome = "Christopher Nolan", NumeroFilmes = 8 },
+                    new Diretor { Nome = "Scott Derrickson", NumeroFilmes = 4 },
+                    new Diretor { Nome = "Gareth Edwards", NumeroFilmes = 3 },
+                    new Diretor { Nome = "Justin Kurzel", NumeroFilmes = 6 }
+                },
+                Filmes = new List<Filme> {
+                    new Filme {
+                        Diretor = new Diretor { Nome = "James Cameron", NumeroFilmes = 5 },
+                        Titulo = "Avatar",
+                        Ano = "2009"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Francis Lawrence", NumeroFilmes = 3 },
+                        Titulo = "I Am Legend",
+                        Ano = "2007"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Zack Snyder", NumeroFilmes = 6 },
+                        Titulo = "300",
+                        Ano = "2006"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Joss Whedon", NumeroFilmes = 7 },
+                        Titulo = "The Avengers",
+                        Ano = "2012"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Martin Scorsese", NumeroFilmes = 4 },
+                        Titulo = "The Wolf of Wall Street",
+                        Ano = "2013"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Christopher Nolan", NumeroFilmes = 8 },
+                        Titulo = "Interstellar",
+                        Ano = "2014"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Game of Thrones",
+                        Ano = "2011–"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Vikings",
+                        Ano = "2013–"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Gotham",
+                        Ano = "2014–"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Power",
+                        Ano = "2014–"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Narcos",
+                        Ano = "2015–"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Breaking Bad",
+                        Ano = "2008–2013"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Scott Derrickson", NumeroFilmes = 4 },
+                        Titulo = "Doctor Strange",
+                        Ano = "2016"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Gareth Edwards", NumeroFilmes = 3 },
+                        Titulo = "Rogue One: A Star Wars Story",
+                        Ano = "2016"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "Justin Kurzel", NumeroFilmes = 6 },
+                        Titulo = "Assassin's Creed",
+                        Ano = "2016"
+                    },
+                    new Filme {
+                        Diretor = new Diretor { Nome = "N/A" },
+                        Titulo = "Luke Cage",
+                        Ano = "2016–"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Cursos_Alura/Parte_9_Json_xml/SerializacaoParte5/ConsoleApp/ConsoleApp/LojaDeFilmes.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace JsonXml
+{
+    [Serializable]
+    [JsonObject("MovieStore")]
+    [DataContract]
+    public class LojaDeFilmes
+    {
+        [JsonProperty("Directors")]
+        
+        public List<Diretor> Diretores = new List<Diretor>();
+        [JsonProperty("Movies")]
+        [DataMember]
+        public List<Filme> Filmes = new List<Filme>();
+        public static void AdicionarFilme(Filme filme)
+        {
+            // Aqui vai a lógica de inserção de filme...
+        }
+    }
+
+
+    [Serializable]
+    [JsonObject("Director")]
+    [DataContract]
+    public class Diretor
+    {
+        [JsonProperty("Name")]
+        public string Nome { get; set; }
+        [JsonIgnore]
+        [NonSerialized]
+        public int NumeroFilmes;
+    }
+
+    [Serializable]
+    [JsonObject("Movie")]
+    [DataContract]
+    public class Filme
+    {
+        [JsonProperty("Director")]
+        public Diretor Diretor { get; set; }
+        [JsonProperty("Title")]
+        [DataMember]
+        public string Titulo { get; set; }
+        [JsonProperty("Year")]
+        [DataMember]
+        public string Ano { get; set; }
+    }
+}
