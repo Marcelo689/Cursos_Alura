1e59ba39ab8b7741af333ef45703af3b7281be7b
Marcelo Jaeger <marcelosj7159@gmail.com>
2022-05-07 19:37:14 -0300

    Realizado projeto uri 1009

---
+++ Cursos_Alura/Desafios_URI/Problema_1009/Problema_1009/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var vendedor = Console.ReadLine();
+        double salario = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double vendasValor = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double salarioMensal = salario + (vendasValor * 0.15); // salário mais 15 %
+        string salarioMensalFormatado = salarioMensal.ToString("F2", CultureInfo.InvariantCulture);
+        Console.WriteLine($"TOTAL = R$ {salarioMensalFormatado}");
+
+        //Console.ReadKey();
+    }
+
+}
