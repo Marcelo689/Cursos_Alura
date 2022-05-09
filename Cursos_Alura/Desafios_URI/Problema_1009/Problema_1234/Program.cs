9ed1cc88851259dbc2eaec6df6406c5ae5b46755
Marcelo Jaeger <marcelosj7159@gmail.com>
2022-05-09 06:57:37 -0300

    Começando parte 3 api rest parte 1

---
+++ Cursos_Alura/Desafios_URI/Problema_1009/Problema_1234/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        //Console.ReadKey();
+        string texto = CapturaTexto();
+        Console.WriteLine(SentecaDancante(texto));
+        Console.ReadKey();
+    }
+
+    public static string SentecaDancante(string entrada)
+    {
+        string saida = "";
+        bool UltimaLetraMaiuscula = true;
+        for (int i = 0; i <= entrada.Length - 1; i++)
+        {
+            if (entrada[i] == ' ')
+            {
+                saida += entrada[i];
+                continue;
+            }
+            else UltimaLetraMaiuscula = !UltimaLetraMaiuscula;
+
+            if (UltimaLetraMaiuscula)
+                saida += entrada[i].ToString().ToLower();
+            else
+                saida += entrada[i].ToString().ToUpper();
+        }
+        if (saida.Length == 0) return "";
+        return saida;
+    }
+
+    static string CapturaTexto()
+    {
+        string texto = "";
+        while(texto.Trim().Length < 50)
+        {
+            texto +=  Console.ReadLine();
+        }
+
+        return texto;
+
+    }
+
+}
---
+++ Cursos_Alura/Desafios_URI/Problema_1009/Problema_1244/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Problema_1244
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] linhas = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                linhas[i] = Console.ReadLine();
+            }
+
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(RetornaLinhaOrdenada(linha));
+            }
+
+            //Console.ReadKey();
+        }
+
+
+        static string RetornaLinhaOrdenada(string linha)
+        {
+            string linhaSaida = "";
+            string maiorPalavra;
+            while (linha.Trim().Length >= 1)
+            {
+                maiorPalavra = RetornaMaiorPalavraApenas(linha);
+                linha = RecortaPalavraDaString(linha, maiorPalavra);
+                linhaSaida += maiorPalavra.Trim() + " ";
+            }
+            return linhaSaida.TrimEnd();
+
+        }
+
+        private static string RetornaMaiorPalavraApenas(string linha)
+        {
+            string[] palavras = linha.Split(' ');
+            string maiorPalavra = "";
+            int maior = 0;
+
+
+            for (int i = 0; i < palavras.Length; i++)
+            {
+                if (palavras[i].Length > maior)
+                {
+                    maior = palavras[i].Length;
+                    maiorPalavra = palavras[i];
+                }
+            }
+
+            return maiorPalavra;
+        }
+
+        static string RecortaPalavraDaString(string linha, string palavra)
+        {
+            string saida = "";
+            int indice = linha.IndexOf(palavra);
+            if (indice != -1)
+            {
+                for (int i = 0; i < linha.Length; i++)
+                {
+                    if ((i < indice || i > indice + palavra.Length - 1))
+                        saida += linha[i];
+                }
+
+            }
+            return saida.Replace("  ", " ");
+
+        }
+    }
+}
