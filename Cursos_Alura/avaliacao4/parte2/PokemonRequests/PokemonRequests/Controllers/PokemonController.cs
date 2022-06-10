39b53f9cd3dddb95311ebc27c9ed815bdac660bd
Marcelo Jaeger <marcelosj7159@gmail.com>
2022-06-10 06:21:20 -0300

    Adicionando Desafio pokemon

---
+++ Cursos_Alura/avaliacao4/parte2/PokemonRequests/PokemonRequests/Controllers/PokemonController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Pokemons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonRequests.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private PokemonContext _context;
+        List<Pokemon> _pokemons = new List<Pokemon>();
+
+        public PokemonController(PokemonContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Adicionar")]
+        public IActionResult Adicionar([FromBody] Pokemon pokemon)
+        {
+            _pokemons.Add(pokemon);
+            Console.WriteLine(pokemon.Nome);
+            _context.Pokemon.Add(pokemon);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public Pokemon EncontrarPokemon([FromRoute] int id)
+        {
+            var pokemon = _context.Pokemon.Find(id);
+            return pokemon;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarPokemon([FromRoute] int id)
+        {
+            var pokemon = _context.Pokemon.Find(id);
+            _context.Pokemon.Remove(pokemon);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Cursos_Alura/avaliacao4/parte2/PokemonRequests/PokemonRequests/PokemonContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pokemons
+{
+    public class PokemonContext : DbContext
+    {
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public PokemonContext(DbContextOptions<PokemonContext> opt) : base (opt)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost;Database=Pokemon;user=root;password=root");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
